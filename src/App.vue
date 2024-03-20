9b9c26570635bde55a4d9b1dd7a2c08c336d33ff
darkhacker1906 <singhabhinandan634@gmail.com>
2024-03-20 15:12:22 +0530

    Initial setup

---
+++ src/App.vue
@@ -0,0 +1,19 @@
+<template>
+<login-comp-vue/>
+  
+</template>
+
+<script>
+import LoginCompVue from './components/LoginComp.vue'
+export default {
+  name:"App",
+  components:{
+    LoginCompVue,
+  }
+
+}
+</script>
+
+<style>
+
+</style>
---
+++ src/components/LoginComp.vue
@@ -0,0 +1,25 @@
+<template>
+  <div>
+    <h1>Input form</h1>
+    <div>
+      <form>
+        <v-text-field label="First name"></v-text-field>
+      </form>
+    </div>
+  </div>
+</template>
+<script>
+
+export default {
+name:"LoginComp"
+}
+</script>
+
+<style>
+h1{
+    text-align: center;
+    margin-top: 30px;
+    margin-bottom: 10px;
+}
+
+</style>
